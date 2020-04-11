c364813d26de873c134723d849458963ff07dbf2
Julian andres montoya carvajal <julian.montoya2@udea.edu.co>
2020-04-11 00:04:18 -0500

    complementando el curso

---
+++ Python/Mimodulo.py
@@ -0,0 +1,16 @@
+"""Mimodulo es un modulo de practica."""
+
+def mi_funcion():
+    "la siguiente funcion me imprime que es una funcion"
+    print("una funcion")
+
+class MiClase:
+    def __init__(self):
+        print("una clase")
+    
+print("un modulo")
+
+def suma(x,y):
+    "la siguiente funcion se encarga de rebibir dos argumentos y retornar la suma"
+    print("suma de dos elementos")
+    return x+y
